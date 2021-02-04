a7af19a49b04e68fba17622b1da65e56075f7b27
anonymousdldh <dlehgo1414@yonsei.ac.kr>
2021-02-04 14:15:35 +0900

    initial commit

---
+++ src/datasets_list.py
@@ -0,0 +1,25 @@
+datasets = {}
+
+datasets['spring_None'] = {
+    'ns':50000,
+    'n':5,
+    'dim':2,
+    'nt':99*5,
+    'dt':5e-3,
+    'sim':'spring_None',
+    'nr':2,
+    't_seen':99,
+    't_interval':5,
+}
+
+datasets['cmu_walking'] = {
+    'ns':92,
+    'n':31,
+    'dim':3,
+    'nt':50*5,
+    'dt':5e-2,
+    'sim':'cmu_walking',
+    'nr':1,
+    't_seen':50,
+    't_interval':1,
+}
